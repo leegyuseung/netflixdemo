e269dedd249d5628ac50ddc72217306aa1029fd2
lee gyuseung <gyus0421@gmail.com>
2024-02-25 01:07:23 +0900

    Banner 만들기

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Banner from "./components/Banner/Banner";
 
-function Homepage() {
-  return <div>Homepage</div>;
-}
+// 1. 배너 => popular 영화를 들고와서 첫 번째 아이템을 보여주자
+// 2. popular movie
+// 3. top rated movie
+// 4. upcoming movie
+const Homepage = () => {
+  return <Banner />;
+};
 
 export default Homepage;
