a3a64ea7cd1b3da44571aaa2114481cf3ad3536a
lee gyuseung <gyus0421@gmail.com>
2024-02-25 03:41:28 +0900

    Banner 디자인하기

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,10 +1,34 @@
+import "./Banner.style.css";
 import React from "react";
+import Alert from "react-bootstrap/Alert";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 
 const Banner = () => {
-  const { data } = usePopularMoviesQuery();
-  console.log("ddd", data);
-  return <div>Banner</div>;
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  if (isLoading) {
+    <h1>Loading.... </h1>;
+  }
+
+  if (isError) {
+    <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${data?.results[0].poster_path}` +
+          ")",
+      }}
+      className="banner"
+    >
+      <div className="text-white banner-text-area">
+        <h1>{data?.results[0].title}</h1>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
