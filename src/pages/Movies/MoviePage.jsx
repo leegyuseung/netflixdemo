f5e1258d801b1ef12458297ca38e57f8486b5da2
lee gyuseung <gyus0421@gmail.com>
2024-02-25 22:29:41 +0900

    키워드로 영화 찾기

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import MovieCard from "../../common/MovieCard/MovieCard";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Alert, Col, Container, Row } from "react-bootstrap";
 
+// 1. nav바에서 클릭해서 온 경우 -> popular movie 보여주기
+// 2. keyword를 입력해서 온 경우 -> keyword와 관련된 영화들을 보여줌
+
+// 페이지네이션 설치
+// page state 만들기
+// 페이지네이션 클릭할때마다 page 바꿔주기
+// page 값이 바뀔때 마다 useSearchMovie에 page까지 넣어서 fetch
 function MoviePage() {
-  return <div>MoviePage</div>;
+  const [query, setQuery] = useSearchParams();
+  const keyword = query.get("q");
+  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+
+  if (isLoading) {
+    <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+  return (
+    <Container>
+      <Row>
+        <Col lg={4} xs={12}>
+          필터
+        </Col>
+        <Col lg={8} xs={12}>
+          <Row>
+            {data.results.map((movie, index) => (
+              <Col key={index} lg={4} xs={12}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default MoviePage;
