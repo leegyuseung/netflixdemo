c1739775712aaea10c42f1e0da53250c3b64ae04
lee gyuseung <gyus0421@gmail.com>
2024-02-25 22:41:40 +0900

    페이지네이션

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieCard from "../../common/MovieCard/MovieCard";
+import ReactPaginate from "react-paginate";
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
 import { Alert, Col, Container, Row } from "react-bootstrap";
@@ -13,8 +14,16 @@ import { Alert, Col, Container, Row } from "react-bootstrap";
 // page 값이 바뀔때 마다 useSearchMovie에 page까지 넣어서 fetch
 function MoviePage() {
   const [query, setQuery] = useSearchParams();
+  const [page, setPage] = useState(1);
+
   const keyword = query.get("q");
-  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+  const { data, isLoading, isError, error } = useSearchMovieQuery({
+    keyword,
+    page,
+  });
+  const handlePageClick = ({ selected }) => {
+    setPage(selected + 1);
+  };
 
   if (isLoading) {
     <h2>Loading...</h2>;
@@ -31,12 +40,33 @@ function MoviePage() {
         </Col>
         <Col lg={8} xs={12}>
           <Row>
-            {data.results.map((movie, index) => (
+            {data?.results.map((movie, index) => (
               <Col key={index} lg={4} xs={12}>
                 <MovieCard movie={movie} />
               </Col>
             ))}
           </Row>
+          <ReactPaginate
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={2}
+            pageCount={data?.total_pages} // 전체 페이지
+            previousLabel="< previous"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+            forcePage={page - 1}
+          />
         </Col>
       </Row>
     </Container>
